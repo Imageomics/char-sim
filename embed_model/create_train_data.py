42d4848f6747d67d73e3e9319919348b45932f1e
KSoumya <ksoumya2301@gmail.com>
2024-08-21 13:21:26 -0400

    Included checks for the number of duplicate pairs

--- embed_model/create_train_data.py
+++ embed_model/create_train_data.py
@@ -2,11 +2,14 @@ import warnings
 warnings.filterwarnings('ignore')
 
 import pandas as pd
+import numpy as np
 
 pheno_desc = pd.read_csv('extracted-descriptions.tsv', sep='\t')
 print("descriptions loaded!")
 
 idStr = pheno_desc['?id'].values
+print("Number of unique trait descriptions: ", 
+      len(pheno_desc.duplicated(keep='first')==True))
 
 for i in range(0, len(idStr)):
     idStr[i] = ''.join(('<', idStr[i], '>'))
@@ -19,6 +22,10 @@ df1 = pd.read_csv('pairwise-sim.tsv.gz', compression='gzip', header=None, sep='\
 print("pairwise ids and scores loaded!")
 
 df1.columns = ['id_1', 'id_2', 'maxIC', 'jaccard', 'simGIC']
+nonDup_org = len(df1.duplicated(subset=['id_1','id_2'], keep='first'))
+print("Total number of pairs in input data, pairwise-sim.tsv.gz: ", len(df1))
+print("Number of non-duplicate pairs in input data, pairwise-sim.tsv.gz: ", nonDup_org)
+print("Number of duplicate pairs in input data, pairwise-sim.tsv.gz: ", len(df1)-nonDup_org)
 
 df1['order'] = range(len(df1))
 merged_df_2 = pd.merge(df1[['order','id_1', 'id_2', 'simGIC']], pheno_desc, on='id_2', how='inner')
@@ -35,6 +42,11 @@ id_1_df.columns = ['id_1', 'id_2_x', 'simGIC_1', 'id_2_y', 'desc_1']
 
 id12_desc12_simGIC = pd.concat([id_1_df[['id_1', 'desc_1', 'simGIC_1']], 
                                 merged_df_2[['id_2', 'desc_2']]], axis = 1)
+descunq=len(id12_desc12_simGIC.duplicated(subset = ['desc_1','desc_2'], keep='first')==True)
+
+print("Total number of pairs in train data, id12_desc12_simGIC: ", len(id12_desc12_simGIC))
+print("Number of non-duplicate pairs in train data, id12_desc12_simGIC: ", descunq)
+print("Number of duplicate pairs in input data, pairwise-sim.tsv.gz: ", len(id12_desc12_simGIC)-descunq)
 
 id12_desc12_simGIC.to_csv("id12_desc12_simGIC.tsv.gz", compression='gzip', sep='\t')
 
